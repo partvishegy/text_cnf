72e75fc6a55226abe4520325aeb846856543480b
partvishegy <venczeli.jozsef@gmail.com>
2019-02-19 14:49:10 +0100

    fix typo

--- vim/install_plugins.sh
+++ vim/install_plugins.sh
@@ -5,11 +5,11 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/colors
 
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-curl ~/.vim/colors/sublimemonokai.vim -LSso https://raw.githubusercontent.com/ErichDonGubler/vim-sublime-monokai/master/colors/sublimemonokai.vim
+curl -LSso ~/.vim/colors/sublimemonokai.vim  https://raw.githubusercontent.com/ErichDonGubler/vim-sublime-monokai/master/colors/sublimemonokai.vim
 
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
-git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle ~/.vim/bundle/vim-fugitive
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
 
